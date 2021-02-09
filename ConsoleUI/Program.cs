c7076e6a8fdefa2eac6625b6cfb48852d3ff0af1
kevsernury <kevsernuryildiz@hotmail.com.tr>
2021-02-09 17:54:15 +0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            carManager.GetAll(); //Bu bize bir liste döndürecek
+
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            carManager.Add(new Entities.Concrete.Car {Id = 10, BrandId= 3, ColorId = 15, DailyPrice = 500, Description = "bişey", ModelYeaer = 1990 });
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        //Constucter method
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, BrandId = 1, ColorId = 15, ModelYeaer = 2015, DailyPrice = 100, Description = "Ford Fiesta"},
+                new Car{Id = 2, BrandId = 1, ColorId = 10, ModelYeaer = 2018, DailyPrice = 130, Description = "Ford Focus"},
+                new Car{Id = 3, BrandId = 2, ColorId = 15, ModelYeaer = 2016, DailyPrice = 140, Description = "Volkswagen Golf"},
+                new Car{Id = 4, BrandId = 2, ColorId = 20, ModelYeaer = 2017, DailyPrice = 175, Description = "Volkswagen Passat"},
+                new Car{Id = 5, BrandId = 3, ColorId = 10, ModelYeaer = 2017, DailyPrice = 110, Description = "Peugeot 308"},
+                new Car{Id = 6, BrandId = 4, ColorId = 15, ModelYeaer = 2018, DailyPrice = 185, Description = "Toyota Corolla"},
+                new Car{Id = 7, BrandId = 5, ColorId = 15, ModelYeaer = 2020, DailyPrice = 220, Description = "Honda Civic"},
+            };
+        }
+         public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate;
+            carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYeaer = car.ModelYeaer;
+        }
+    }
+}
